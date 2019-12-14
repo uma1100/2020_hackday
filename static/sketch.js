76e3b34b89a1e0dd564f7e17abb59b54f93da03a
Kazuki_Miya <fwpf1766@gmail.com>
2019-12-15 03:38:23 +0900

    なんか新しいの

--- static/sketch.js
+++ static/sketch.js
@@ -1,8 +1,7 @@
-
-// example progressbar 
+// example progressbar
 // default variables for progressbar
 // start the time for progressbar
-// boolean for the end progressbar 
+// boolean for the end progressbar
 var done;
 
 var hitpoint;
@@ -35,31 +34,22 @@ function sleep(waitMsec) {
   while (new Date() - startMsec < waitMsec);
 }
 // draw all text and progressbar
+var circle_array = []; // 円の色、位置、半径を記録する用の配列
 function draw() {
   // set same background color
   background(255);
 
-  // check end of progressbar fill
   done = true;
-  // create the color for fill progressbar
-  fill(110, 110, 255);
-  // no stroke for draw
   noStroke();
-  // show all text variables and progressbar
-  text("Progress bar blue - size 620", 230, 20);
-  fill('red');
-  rect(width / 2 - 310, height / 2, 620, 30);
-  fill('blue');
-  // rect(width / 2 - 310, height / 2, hitpoint, 30);
-  hitpoint += evaluation;
-  count++;
-  if (count > Math.abs(evaluation)) {
-    evaluation = 0;
-  }
 
-  rect(width / 2 - 310, height / 2, hitpoint, 30);
-  // evaluation = 0;
-  // console.log(hitpoint);
+  // 円の描写 ここから
+  for (let [cloor, x, y, r] of circle_array) {
+    fill(cloor);
+    ellipse(x, y, r, r);
+  }
+  text("Progress bar blue - size 620", 230, 20);
+  //ここまで
+
   if (hitpoint >= hp_width) {
     console.log("finish");
     finish_notice(3);
@@ -69,23 +59,30 @@ function draw() {
     console.log("bad finish");
     finish_notice(2);
     noLoop();
-
   }
   sleep(300);
-
 
   // rect(width/2-310,height/2,map(counter-startTime,0,maxTime,0,310), 30 );
   // text("counter- startTime "+int(counter- startTime)+" ",10,80);
   // text("maxTime "+ int(maxTime) +  " ", 10,100);
   // text("map converted counter-startTime"+ int ( map(counter-startTime,0,maxTime,0,200)), 10,120);
   noFill();
-
 }
-// reload the draw of progress bar 
+// reload the draw of progress bar
 function battle(player, enemy) {
   if (done) {
     attack_val = 1;
     bias = 5;
+
+    // 円の登録 ここから
+    var random_x = Math.random() * width;
+    var random_y = Math.random() * height;
+    circle_array.push(["red", random_x, random_y, 20 * enemy]);
+    random_x = Math.random() * width;
+    random_y = Math.random() * height;
+    circle_array.push(["blue", random_x, random_y, 20 * player]);
+    // ここまで
+
     if (enemy < player) {
       // playerのほうが運動量が多いとき
       evaluation = bias * player;
@@ -104,8 +101,8 @@ function battle(player, enemy) {
   }
 }
 
-function mousePressed () { 
-  if (done) { 
+function mousePressed() {
+  if (done) {
     // counter = 0; startTime = millis();
     // attack_val = 1;
     bias = 10;
@@ -115,7 +112,7 @@ function mousePressed () {
     // maxTime = int(random(1000, 1976));
     done = false;
     active = true;
-    count = 0; 
-  done=false;
+    count = 0;
+    done = false;
   }
 }
