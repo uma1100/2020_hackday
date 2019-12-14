c13b4af7af3ed66a7c71b27f0aa529d2649efd43
yozzzo <tennis25971@gmail.com>
2019-12-15 00:11:08 +0900

    hgehge

--- static/socket_client.js
+++ static/socket_client.js
@@ -1,12 +1,12 @@
 var web_socket = null;
 
 function on_load() {
-  web_socket = new WebSocket("ws://192.168.43.195:8080"); // サーバーのアドレスを指定
+  web_socket = new WebSocket("ws://172.20.10.2:8080"); // サーバーのアドレスを指定
   web_socket.binaryType = "arraybuffer";
-  // web_socket.onmessage = on_message;
   console.log("接続ok");
 }
 
 function finish_notice(flag) {
+  // flag = document.getElementById("text_inp);
   web_socket.send(flag);
 }
